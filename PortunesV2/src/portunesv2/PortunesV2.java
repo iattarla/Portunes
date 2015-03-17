228317291ba37b7d590baf1ff1e22e58da9c7360
Bilal Tonga <bilal.tonga@tarla.org.tr>
2015-03-17 10:02:51 +0200

    wiedgand read are done

--- PortunesV2/src/portunesv2/PortunesV2.java
+++ PortunesV2/src/portunesv2/PortunesV2.java
@@ -10,7 +10,6 @@ package portunesv2;
  * @author bilal
  */
 
-import com.pi4j.component.lcd.LCDTextAlignment;
 import com.pi4j.component.lcd.impl.GpioLcdDisplay;
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
@@ -18,17 +17,18 @@ import com.pi4j.io.gpio.RaspiPin;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.text.SimpleDateFormat;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
 public class PortunesV2 {
 
-    public final static int LCD_ROWS = 2;
+    public final static int LCD_ROWS = 4;
     public final static int LCD_ROW_1 = 0;
     public final static int LCD_ROW_2 = 1;
-    public final static int LCD_COLUMNS = 16;
+    public final static int LCD_ROW_3 = 2;
+    public final static int LCD_ROW_4 = 3;
+    public final static int LCD_COLUMNS = 20;
     public final static int LCD_BITS = 4;
     
     
@@ -52,25 +52,35 @@ public class PortunesV2 {
         //wi.begin();
         lcd.clear();
         
+        lcd.write(LCD_ROW_4,"Portunes V2 by TARLA");
         
-        System.out.println("get into while loop");
+        lcd.write(LCD_ROW_1,"Welcome,Card please.");
+        
+        
         while(true){
             
-                
                 try {
                     
-                    //System.out.println(wi.getCode());
-                    
-                    Process p = Runtime.getRuntime().exec("portunes-read");
+                    Process p = Runtime.getRuntime().exec("portunes-read"); // portunes read wiedgard command
                     BufferedReader stdInput = new BufferedReader(new 
                     InputStreamReader(p.getInputStream()));
                     
                     int exitVal = p.waitFor();
-                    String s = null;
+                    String cardnoFull = null;
+                    String cardnoBin = "";
                     
-                    while ((s = stdInput.readLine()) != null) {
-                        System.out.println(s);
-                        lcd.write(LCD_ROW_1, s );
+                    lcd.clear();
+                    
+                    while ((cardnoFull = stdInput.readLine()) != null) {
+                        //s = s.replace("\n", "").replace("\r", "");
+                        
+                        for(int a=1;a<25;a++){ // get data without checksum
+                            cardnoBin = cardnoBin + cardnoFull.charAt(a);
+                        } 
+                        
+                        long card_no = Long.parseLong(cardnoBin, 2);
+                        
+                        lcd.write(LCD_ROW_1, String.valueOf(card_no) );
                     }
                     
                 } catch (IOException ex) {
@@ -79,72 +89,7 @@ public class PortunesV2 {
             
         }
         
-        // clear LCD
         
-        /*
-        // write line 1 to LCD
-        lcd.write(LCD_ROW_1, "The Pi4J Project");
-        // write line 2 to LCD
-        lcd.write(LCD_ROW_2, "----------------");
-        // line data replacement
-        for(int index = 0; index < 5; index++){
-        
-            lcd.write(LCD_ROW_2, "----------------");
-            Thread.sleep(500);
-            lcd.write(LCD_ROW_2, "****************");
-            Thread.sleep(500);
-
-        }
-
-        lcd.write(LCD_ROW_2, "----------------");
-        // single character data replacement
-        
-        for(int index = 0; index < lcd.getColumnCount(); index++) {
-            lcd.write(LCD_ROW_2, index, ">");
-        
-            if(index > 0)
-                lcd.write(LCD_ROW_2, index - 1, "-");
-                
-            Thread.sleep(300);
-        }
-
-        for(int index = lcd.getColumnCount()-1; index >= 0 ; index--) {
-            lcd.write(LCD_ROW_2, index, "<");
-            
-            if(index < lcd.getColumnCount()-1)
-                lcd.write(LCD_ROW_2, index + 1, "-");
-            Thread.sleep(300);
-        }
-
-        // left alignment, full line data
-        lcd.write(LCD_ROW_2, "----------------");
-        Thread.sleep(500);
-        lcd.writeln(LCD_ROW_2, "<< LEFT");
-        Thread.sleep(1000);
-        // right alignment, full line data
-        lcd.write(LCD_ROW_2, "----------------");
-        Thread.sleep(500);
-
-        lcd.writeln(LCD_ROW_2, "RIGHT >>", LCDTextAlignment.ALIGN_RIGHT);
-        Thread.sleep(1000);
-        
-        // center alignment, full line data
-        lcd.write(LCD_ROW_2, "----------------");
-        Thread.sleep(500);
-   
-        lcd.writeln(LCD_ROW_2, "<< CENTER >>", LCDTextAlignment.ALIGN_CENTER);
-        Thread.sleep(1000);
-        
-        // mixed alignments, partial line data
-        lcd.write(LCD_ROW_2, "----------------");
-        Thread.sleep(500);
-        
-        lcd.write(LCD_ROW_2, "<L>", LCDTextAlignment.ALIGN_LEFT);
-        lcd.write(LCD_ROW_2, "<R>", LCDTextAlignment.ALIGN_RIGHT);
-        lcd.write(LCD_ROW_2, "CC", LCDTextAlignment.ALIGN_CENTER);
-        Thread.sleep(3000);
-        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
-        // update time
-    */
+    
     }
 }
